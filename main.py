4b225fb726e9db36dc8304c44e80b7665567b398
Ronnie Nissan <ronnie.nissan@proton.me>
2023-04-04 11:52:32 +0300

    added a boot.py

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.modules.layers import Layers
 from kmk.extensions.stringy_keymaps import StringyKeymaps
+from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.capsword import CapsWord
 
 PlanckHW = KMKKeyboard()
 
 PlanckHW.modules.append(Layers())
+PlanckHW.modules.append(CapsWord())
 PlanckHW.extensions.append(StringyKeymaps())
+PlanckHW.extensions.append(MediaKeys())
 
 LOWER = KC.MO(1)
 RAISE = KC.MO(2)
 xx = "NO"
 __ = "TRNS"
+cut = KC.LCTL(KC.X)
+copy = KC.LCTL(KC.C)
+paste = KC.LCTL(KC.V)
+close = KC.LALT(KC.Z)
 
 # fmt: off
 PlanckHW.keymap = [
@@ -23,16 +31,22 @@ PlanckHW.keymap = [
         'ESC',     'Q',    'W',    'E',   'R',   'T',   'Y',    'U',     'I',     'O',     'P',  'BSPC',
         'TAB',     'A',    'S',    'D',   'F',   'G',   'H',    'J',     'K',     'L',  'SCLN',  'QUOT',
         'LSFT',    'Z',    'X',    'C',   'V',   'B',   'N',    'M',  'COMM',   'DOT',  'SLSH',   'ENT',
-        'BSPC', 'LCTL', 'LALT', 'LGUI', LOWER,    'SPC',    RAISE,    'LEFT',  'DOWN',    'UP', 'RIGHT',
+        'GRAVE', 'LCTL', 'LALT', 'LGUI', LOWER,    'SPC',    RAISE,    'LEFT',  'DOWN',    'UP', 'RIGHT',
     ],
         # layer 1, LOWER
     [
-             __,    '1',    '2',     '3',      '4',      '5',   '6',        '7',       '8',       '9',      '0',      __,
-         ' TAB', 'EXLM',   'AT',  'HASH',    'DLR',   'PERC',   'CIRC',  'AMPR',    'ASTR',    'LPRN',   'RPRN',      __,
+             __,    '1',    '2',     '3',      '4',      '5',      '6',     '7',       '8',       '9',      '0',      __,
+           'CW', 'EXLM',   'AT',  'HASH',    'DLR',   'PERC',   'CIRC',  'AMPR',    'ASTR',    'LPRN',   'RPRN',      __,
          'LSFT', 'TILD',  'EQL',  'PLUS',   'MINS',   'UNDS',   'LBRC',  'RBRC',    'LCBR',    'RCBR',  'BSLSH',  'PIPE',
-        'GRAVE',     __,     __,      __,    LOWER,         'SPC',           xx,        __,        __,       __,      __,
+             xx,     __,     __,      __,       __,         'SPC',           xx,        __,        __,       __,      __,
     ],
-
+        # layer 2, RAISE
+    [
+            'F12',   'F1',    'F2',    'F3',    'F4',    'F5',     'F6',    'F7',      'F8',    'F9',    'F10',  'F11',
+          'RESET', 'MUTE',  'VOLD',  'VOLU',  'BRID',  'BRIU',   'PSCR',  'LEFT',    'DOWN',    'UP',  'RIGHT',     xx,
+         'RELOAD',  close,     cut,    copy,   paste,      xx,    'INS',  'HOME',    'PGDN',  'PGUP',    'END',     xx,
+          'DEBUG',     __,      __,      __,      xx,         'SPC',          __,        __,      __,       __,     __,
+    ],
 ]
 
 
